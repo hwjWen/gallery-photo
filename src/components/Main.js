61967f061414b1604b24c71a7752af8bd7bce293
hwjWen <hwj209_610@163.com>
2017-07-26 00:39:30 +0800

    0.0.1
    add img

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,17 +2,27 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
+// 获取图片路径
+var imageDatas = require('../data/imageData.json');
+// 利用自执型函数，将图片信息转为图片路径
+imageDatas = (function getImageData(img) {
+    for (var i = 0, j = img.length; i < j; i++) {
+        var singleImg = img[i];
+        singleImg.imgUrl = require('../images/' + singleImg.fileName);
+        img[i] = singleImg;
+    }
+    return img;
+})(imageDatas);
 
 class AppComponent extends React.Component {
     render() {
         return (
-            <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        <span>Hello word</span>
-      </div>
+            <section className="stage">
+            <section className="img-sec">
+
+            </section>
+            <nav className="controll-nav"></nav>
+          </section>
         );
     }
 }
